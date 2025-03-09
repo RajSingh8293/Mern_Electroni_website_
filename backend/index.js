2b8cc07c0df84dd44fa9e6b8a8db807bf94dd48e
LearnCode <rajsingh987384@gmail.com>
2025-03-09 18:23:01 +0530

    run npm command

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
 import dbConnect from "./db/conn.js";
